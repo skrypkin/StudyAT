950d51db5ab5b77e8812e48db300a0f2e24eafa7
Viсtor Skrypkin <jaller2469@mail.ru>
2020-04-11 19:30:52 +0300

    day3
    Программа на Java для открытия сайта. Задание от Техносерв. День 3

---
+++ day3/src/test/java/Open_site/Open_site.java
@@ -0,0 +1,28 @@
+package Open_site;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Open_site {    public static void main(String[] args) {
+
+    System.setProperty("webdriver.chrome.driver","D:/java/test_intern/chromedriver.exe");
+    ChromeDriver driver = new ChromeDriver();
+
+    String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String expectedTitle = "Welcome: Mercury Tours";
+    String actualTitle = "";
+    driver.get(baseUrl);
+
+
+    actualTitle = driver.getTitle();
+
+    if (actualTitle.contentEquals(expectedTitle)){
+        System.out.println("Test Passed!");
+    } else {
+        System.out.println("Test Failed");
+    }
+
+    driver.close();
+
+}
+}
