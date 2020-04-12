adfc01458cf792b6d9389a0be00165e0f9106a8a
Viсtor Skrypkin <jaller2469@mail.ru>
2020-04-12 21:53:22 +0300

    Update FirstTest.java

--- sunday_12.04.20/src/test/java/ru/msudrf/lpk/levober1/FirstTest.java
+++ sunday_12.04.20/src/test/java/ru/msudrf/lpk/levober1/FirstTest.java
@@ -1,16 +1,36 @@
 package ru.msudrf.lpk.levober1;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
 
-    @Test
-    public void FirstTest() {
+    public ChromeDriver driver;
+
+    @Before
+    public void SetUp() {
         System.setProperty("webdriver.chrome.driver","D:/java/test_intern/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
+        System.out.println("Test Start!");
+    }
+
+    @Test
+    public void FirstTest() {
 
         driver.get("http://levober1.lpk.msudrf.ru/");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Судебный участок №1 </br>Левобережного судебного района г. Липецка"));
+
+    }
+
+    @After
+    public void Close() {
+        System.out.println("Test Close!");
+        driver.quit();
     }
 
 }
