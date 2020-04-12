fb5de9087ba0c59d5dc0276fc231295b5e4d3895
Viсtor Skrypkin <jaller2469@mail.ru>
2020-04-12 21:26:17 +0300

    .

--- sunday_12.04.20/src/test/java/ru/msudrf/lpk/levober1/FirstTest.java
+++ sunday_12.04.20/src/test/java/ru/msudrf/lpk/levober1/FirstTest.java
@@ -1,4 +1,16 @@
 package ru.msudrf.lpk.levober1;
 
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstTest {
+
+    @Test
+    public void FirstTest() {
+        System.setProperty("webdriver.chrome.driver","D:/java/test_intern/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("http://levober1.lpk.msudrf.ru/");
+    }
+
 }
